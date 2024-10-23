c1009526851473ed8438e54c3e5da70c1cbf9b8c
nnaooee <ppppkh0401@gmail.com>
2024-10-23 21:52:40 +0900

    .

---
+++ 중간과제/script2.js
@@ -0,0 +1,62 @@
+$('#button01').hover(
+    function(){
+        $('#target01').css('display', 'block');
+    },
+    function(){
+        $('#target01').css('display', 'none');
+    }
+);
+$('#button01').hover(
+    function(){
+        $('#target02').css('display', 'block');
+    },
+    function(){
+        $('#target02').css('display', 'none');
+    }
+);
+$('#button01').hover(
+    function(){
+        $('#target03').css('display', 'block');
+    },
+    function(){
+        $('#target03').css('display', 'none');
+    }
+);
+$('#button01').hover(
+    function(){
+        $('#target04').css('display', 'block');
+    },
+    function(){
+        $('#target04').css('display', 'none');
+    }
+);
+$('#button01').hover(
+    function(){
+        $('#target05').css('display', 'block');
+    },
+    function(){
+        $('#target05').css('display', 'none');
+    }
+);
+$('#button01').hover(
+    function(){
+        $('#target06').css('display', 'block');
+    },
+    function(){
+        $('#target06').css('display', 'none');
+    }
+);
+$('#button01').hover(
+    function(){
+        $('#target07').css('display', 'block');
+    },
+    function(){
+        $('#target07').css('display', 'none');
+    }
+);
+
+$('#button01').click(
+    function(){
+        $('#target01').css({'width':'400px', 'height':'400px'});
+    }
+);
