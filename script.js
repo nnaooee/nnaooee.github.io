e6f3daecad23ae6c8857be07e540f890bd6aef15
nnaooee <ppppkh0401@gmail.com>
2024-10-23 10:54:47 +0900

    .

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-gsap.registerPlugin(Flip);
-
-let button = document.querySelector("button");
-
-button.addEventListener("click", function() {
-  // get the current state (before we make changes)
-  let state = Flip.getState(element, "backgroundColor");
-  
-  // now make our changes
-  element.classList.toggle("absolute");
-  element.innerText = element.classList.contains("absolute") ?  "absolute" : "relative";
-  
-  // "FLIP" animate from that previous state. 
-  Flip.from(state, {
-    duration: 1,
-    ease: "power1.inOut",
-    spin: 2,
+const spin = gsap.timeline({repeat:-1})
+  .set("#svg-stage", { opacity: 1 })
+  .fromTo("#clover", {
+    transformOrigin: "50%",
+    x: 30,
+    y: 30
+  },{
+    duration: 50,
+    rotation: 360,
+    ease: "none",
   });
-  
+
+Draggable.create("#clover", {
+  type: "rotation",
+  trigger: "#svg-stage",
+  inertia: true,
+  onPressInit: () => spin.pause(),
+  onDrag: setSpinProgress,  // on drag & throw, use rotation as a progress value for the spin timeline
+  onThrowUpdate: setSpinProgress,
+  onThrowComplete: () => {
+    spin.resume();
+    gsap.fromTo(spin, { timeScale: 0 }, { timeScale: 1, ease: "power1.in" });
+  }
 });
+
+function setSpinProgress() {
+  spin.progress(gsap.utils.wrap(0, 360, this.rotation) / 360);
+}
